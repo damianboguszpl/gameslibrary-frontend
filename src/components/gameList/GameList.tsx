fad015d03adbdb2e09bc745ae959a2da6dd16b17
Paweł Ciszewski <kardahim@gmail.com>
2022-11-25 14:39:34 +0100

    add rwd

--- src/components/gameList/GameList.tsx
+++ src/components/gameList/GameList.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Stack } from '@mui/material';
+import { Button, Container, Paper, Stack } from '@mui/material';
 import React from 'react'
 import { useEffect, useState } from 'react'
 import axios from '../../api/axios.js';
@@ -12,13 +12,11 @@ function GameList() {
     const [lastIndex, setLastIndexIndex] = useState(0)
     const pageLength = 20
 
-
     useEffect(() => {
         axios.get('/app').then((response) => setApps(Array.from({ length: pageLength }, (v, i) => {
             if (i === pageLength - 1) {
                 setLastIndexIndex(i + 1)
             }
-            // console.log(i)
             return response.data[i]
         })))
     }, [])
@@ -28,7 +26,6 @@ function GameList() {
             if (i === pageLength - 1) {
                 setLastIndexIndex(lastIndex + i + 1)
             }
-            // console.log(lastIndex + i)
             return response.data[lastIndex + i]
         }))))
     }
@@ -40,14 +37,13 @@ function GameList() {
             loader={<h4>Loading...</h4>}>
             <Stack className='game_list' spacing={3}>
                 {apps?.map((value, key) =>
-                    <Stack spacing={10} direction='row' className='game_list__row' key={key}>
+                    <Paper className='game_list__row' key={key} elevation={3}>
                         <img alt={value.title} src={value.screenshotLink} className='game_list__row__image' loading="lazy" />
                         <div className='game_list__row__details'>
                             <h1 className='game_list__row__title'>{value.title}</h1>
                             <p className='game_list__row__description'>{value.shortDescription}</p>
-                            <Button className='game_list__row__button'>Details</Button>
                         </div>
-                    </Stack>
+                    </Paper>
                 )}
             </Stack>
         </InfiniteScroll>
