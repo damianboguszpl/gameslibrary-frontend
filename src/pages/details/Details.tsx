316b9953bf54bdf7b64e844fefe639be018aea44
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-12-03 01:36:46 +0100

    add conditionally rendered button for navigation to new-review form

--- src/pages/details/Details.tsx
+++ src/pages/details/Details.tsx
@@ -1,15 +1,20 @@
-import { Box, Container, Paper } from '@mui/material';
-import React, { Suspense, useEffect, useState } from 'react'
+import { Box, Container, Paper, Button } from '@mui/material';
+import React, { Suspense, useEffect, useState, useContext } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+import useAxiosPrivate from '../../hooks/useAxiosPrivate'
+import { AuthContext } from '../../context/AuthContext'
 import axios from '../../api/axios.js';
 import './Details.scss';
 const Favorites = React.lazy(() => import('../../components/favorites/Favorites'));
 
 function Details() {
     const navigate = useNavigate()
+    const axiosPrivate = useAxiosPrivate()
 
+    const context = useContext(AuthContext)
     const { id } = useParams()
     const [app, setApp] = useState<any>()
+    const [review, setReview] = useState<any>()
 
     useEffect(() => {
         axios.get(`/app/${id}`).then((response) => {
@@ -21,15 +26,29 @@ function Details() {
                 response.data.publishers = JSON.parse(response.data.publishers)
                 response.data.developers = JSON.parse(response.data.developers)
                 setApp(response.data)
+
+                axiosPrivate.get(`/review/user/${context?.authState.id}/app/${id}`, {
+                    withCredentials: false
+                }).then((response) => { 
+                    if (response.status === 200)
+                        setReview(response.data) 
+                })
             }
         })
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    }, [context])
 
     return (
         <Container maxWidth="md" className='details'>
             <Paper elevation={4} className='details__card'>
                 <Suspense fallback={'Loading...'}><Favorites app_id={app?.id} /></Suspense>
+                <Suspense fallback={'Loading...'}> 
+                { !review && (
+                    <Button className='add_review__button' variant='contained' type='submit' onClick={()=> { navigate(`/new-review/${id}`) }}>
+                        Add a review
+                    </Button>
+                )}
+                </Suspense>
                 <Box className='details__card__header'>
                     {app?.title}
                 </Box>
