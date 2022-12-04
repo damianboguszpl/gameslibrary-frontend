66cac9c58b0e3d31d101a9893e6ea03d6fc02587
Paweł Ciszewski <kardahim@gmail.com>
2022-12-04 17:56:14 +0100

    add Reviews list

--- src/pages/details/Details.tsx
+++ src/pages/details/Details.tsx
@@ -1,10 +1,11 @@
-import { Box, Container, Paper, Button } from '@mui/material';
+import { Box, Container, Paper, Button, Stack } from '@mui/material';
 import React, { Suspense, useEffect, useState, useContext } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import useAxiosPrivate from '../../hooks/useAxiosPrivate'
 import { AuthContext } from '../../context/AuthContext'
 import axios from '../../api/axios.js';
 import './Details.scss';
+import '../reviewList/ReviewList.scss'
 const Favorites = React.lazy(() => import('../../components/favorites/Favorites'));
 
 function Details() {
@@ -15,6 +16,7 @@ function Details() {
     const { id } = useParams()
     const [app, setApp] = useState<any>()
     const [review, setReview] = useState<any>()
+    const [allReviews, setAllReviews] = useState<any[]>([])
 
     useEffect(() => {
         axios.get(`/app/${id}`).then((response) => {
@@ -29,25 +31,33 @@ function Details() {
 
                 axiosPrivate.get(`/review/user/${context?.authState.id}/app/${id}`, {
                     withCredentials: false
-                }).then((response) => { 
+                }).then((response) => {
                     if (response.status === 200)
-                        setReview(response.data) 
+                        setReview(response.data)
+                })
+                axiosPrivate.get(`/review/app/${id}`, {
+                    withCredentials: false
+                }).then((response) => {
+                    if (response.status === 200)
+                        setAllReviews(response.data)
+                    else setAllReviews([])
+                    console.log(response.data)
                 })
             }
         })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [context])
 
     return (
         <Container maxWidth="md" className='details'>
             <Paper elevation={4} className='details__card'>
                 <Suspense fallback={'Loading...'}><Favorites app_id={app?.id} /></Suspense>
-                <Suspense fallback={'Loading...'}> 
-                { !review && (
-                    <Button className='add_review__button' variant='contained' type='submit' onClick={()=> { navigate(`/new-review/${id}`) }}>
-                        Add a review
-                    </Button>
-                )}
+                <Suspense fallback={'Loading...'}>
+                    {!review && (
+                        <Button className='add_review__button' variant='contained' type='submit' onClick={() => { navigate(`/new-review/${id}`) }}>
+                            Add a review
+                        </Button>
+                    )}
                 </Suspense>
                 <Box className='details__card__header'>
                     {app?.title}
@@ -71,6 +81,27 @@ function Details() {
                         Developers: {app?.developers.map((developer: string, key: number) => (key === 0 ? `${developer}` : `, ${developer}`))}
                     </strong>
                 </Box>
+            </Paper>
+            <Paper className='details__card details__card--top_margin'>
+                <Box className='details__card__header'>
+                    Recenzje
+                </Box>
+                <Stack className='review_list' spacing={3}>
+                    {allReviews?.map((value, key) =>
+                        <Paper className='review_list__row' key={key} elevation={3} >
+                            <img alt={value.app.title} src={value.app.screenshotLink} className='review_list__row__image' loading="lazy" onClick={() => navigate(`/details/${value.app.id}`)} />
+                            <div className='review_list__row__details'>
+                                <h1 className='review_list__row__title' onClick={() => navigate(`/details/${value.app.id}`)}>{value.app.title}</h1>
+                                <div>
+                                    <p className='review_list_row_rating'><strong>User: </strong>{value.user.login}</p>
+                                    <p className='review_list_row_rating'><strong>Rating: </strong>{value.rating}</p>
+                                    <strong>Text Review: </strong>
+                                    <p className='review_list_row_text_review'>{value.textReview}</p>
+                                </div>
+                            </div>
+                        </Paper>
+                    )}
+                </Stack>
             </Paper>
         </Container>
     )
